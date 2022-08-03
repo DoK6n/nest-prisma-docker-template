51df3c11c33315cbca8d2995cb873d15d37799b0
Dok6n <ehrbs2018@gmail.com>
2022-08-03 22:29:44 +0900

    Add rest api

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Controller('articles')
+@ApiTags('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
+  async create(@Body() createArticleDto: CreateArticleDto) {
+    return await this.articlesService.create(createArticleDto);
+  }
+
+  @Get('drafts')
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  async findDrafts() {
+    return await this.articlesService.findDrafts();
+  }
+
+  @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  async findAll() {
+    return await this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  async findOne(@Param('id') id: string) {
+    return await this.articlesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  async update(
+    @Param('id') id: string,
+    @Body() updateArticleDto: UpdateArticleDto,
+  ) {
+    return await this.articlesService.update(+id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  async remove(@Param('id') id: string) {
+    return await this.articlesService.remove(+id);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
+
   const logger = new Logger('START');
   logger.log('서버 3001 포트로 실행중...');
 }
