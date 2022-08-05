2ec0fd635c178fa73eafbeb63c4b637c3e78fbc3
Dok6n <ehrbs2018@gmail.com>
2022-08-05 16:50:31 +0900

    Add graphQL example app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   const logger = new Logger('START');
-  logger.log('서버 3001 포트로 실행중...');
+  await app.listen(3001, () => {
+    logger.log(`
+
+              🚀 Server ready at: http://localhost:3001
+              ⭐️ Reference :
+                  - blog post           : https://www.prisma.io/blog
+                  - prisma examples     : https://github.com/prisma/prisma-examples/tree/latest/typescript/graphql-nestjs
+                  - See sample queries  : http://pris.ly/e/ts/graphql-nestjs#using-the-graphql-api
+
+    `);
+  });
 }
 bootstrap();
